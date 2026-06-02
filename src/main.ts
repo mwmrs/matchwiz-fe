07d908c87d20a5af11dcbf563192ee2105e86f35
M. Wiemers <github@mwmrs.de>
2026-06-02 17:25:18 +0200

    real backend connection

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { environment } from './environments/environment';
 
 async function bootstrap() {
-  if (isDevMode()) {
+  if (isDevMode() && environment.useMocks) {
     const { worker } = await import('./app/mocks/browser');
     await worker.start({ onUnhandledRequest: 'bypass' });
   }
